d937e390931e1d119ba777d7e1fa2b41e6a9876f
Daniel Jackson <daniel.jackson@dialexa.com>
2015-10-16 11:11:27 -0500

    Converting server to http

--- pylab_server.py
+++ pylab_server.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.7
 
-import SocketServer
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from SocketServer import ThreadingMixIn
+import threading
 import visa
 import json
 import base64
@@ -37,48 +39,57 @@ class InstrumentManager:
 
 insman = InstrumentManager()
 
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
 
-class InstrumentRequestHandler(SocketServer.StreamRequestHandler):
-    def handle(self):
-        line = self.rfile.readline().strip()
-        command = line.split(' ')[0]
-        if command == 'list':
-            self.wfile.write(json.dumps(self.list_instruments())+'\r\n')
-        elif command == 'open':
-            identifier = line.split(' ')[1]
-            self.wfile.write(json.dumps(self.open_instrument(identifier))+'\r\n')
-        elif command == 'write':
-            identifier = line.split(' ')[1]
-            query = ' '.join(line.split(' ')[2:])
-            self.wfile.write(json.dumps(self.write_instrument(identifier, query))+'\r\n')
-        elif command == 'query':
-            identifier = line.split(' ')[1]
-            query = ' '.join(line.split(' ')[2:])
-            self.wfile.write(json.dumps(self.query_instrument(identifier, query))+'\r\n')
-        elif command == 'queryb':
-            identifier = line.split(' ')[1]
-            query = ' '.join(line.split(' ')[2:])
-            self.wfile.write(json.dumps(self.query_instrument_binary(identifier, query))+'\r\n')
+#class InstrumentRequestHandler(SocketServer.StreamRequestHandler):
+#    def handle(self):
+#        line = self.rfile.readline().strip()
+#        command = line.split(' ')[0]
+#        if command == 'list':
+#            self.wfile.write(json.dumps(self.list_instruments())+'\r\n')
+#        elif command == 'open':
+#            identifier = line.split(' ')[1]
+#            self.wfile.write(json.dumps(self.open_instrument(identifier))+'\r\n')
+#        elif command == 'write':
+#            identifier = line.split(' ')[1]
+#            query = ' '.join(line.split(' ')[2:])
+#            self.wfile.write(json.dumps(self.write_instrument(identifier, query))+'\r\n')
+#        elif command == 'query':
+#            identifier = line.split(' ')[1]
+#            query = ' '.join(line.split(' ')[2:])
+#            self.wfile.write(json.dumps(self.query_instrument(identifier, query))+'\r\n')
+#        elif command == 'queryb':
+#            identifier = line.split(' ')[1]
+#            query = ' '.join(line.split(' ')[2:])
+#            self.wfile.write(json.dumps(self.query_instrument_binary(identifier, query))+'\r\n')
+#
+#    def list_instruments(self):
+#        return insman.list_instruments()
+#
+#    def open_instrument(self, identifier):
+#        return insman.open_instrument(identifier)
+#
+#    def write_instrument(self, identifier, query):
+#        return insman.write_instrument(identifier, query)
+#
+#    def query_instrument(self, identifier, query):
+#        return insman.query_instrument(identifier, query).strip()
+#
+#    def query_instrument_binary(self, identifier, query):
+#        return base64.b64encode(insman.query_instrument_binary(identifier, query))
 
-    def list_instruments(self):
-        return insman.list_instruments()
 
-    def open_instrument(self, identifier):
-        return insman.open_instrument(identifier)
-
-    def write_instrument(self, identifier, query):
-        return insman.write_instrument(identifier, query)
-
-    def query_instrument(self, identifier, query):
-        return insman.query_instrument(identifier, query).strip()
-
-    def query_instrument_binary(self, identifier, query):
-        return base64.b64encode(insman.query_instrument_binary(identifier, query))
-
+class InstrumentRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print self.rfile.read()
+        self.send_response(200)
+        self.end_header()
+        self.wfile.write("hello\n")
+        return
 
 if __name__ == '__main__':
     HOST, PORT = "0.0.0.0", 9090
 
-    server = SocketServer.TCPServer((HOST, PORT), InstrumentRequestHandler)
-
+    server = ThreadedHTTPServer((HOST, PORT), InstrumentRequestHandler)
     server.serve_forever()
